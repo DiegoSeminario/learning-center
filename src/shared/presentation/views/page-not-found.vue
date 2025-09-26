3bfb6952095fd970c225479b9c3a28c7a8134d54
DiegoSeminario <u202412591@upc.edu.pe>
2025-09-26 16:41:02 -0500

    feat: enhance page-not-found component with i18n support and router integration

--- src/shared/presentation/views/page-not-found.vue
+++ src/shared/presentation/views/page-not-found.vue
@@ -1,9 +1,19 @@
-<script setup lang="ts">
+<script setup>
+import {useI18n} from "vue-i18n";
+import {useRouter} from "vue-router";
+
+const {t } = useI18n();
+const router = useRouter();
+const unavailableRoute = router.currentRoute.value.fullPath;
 
 </script>
 
 <template>
-
+<div class="align-content-start justify-content-start m-4">
+  <h1>{{ t('pageNotFound.title') }}</h1>
+  <p>{{ t('pageNotFound.content', {'unavailable-router': unavailableRoute})}}</p>
+  <router-link to="/home"> {{t('page-not-found.go-home')}}</router-link>
+</div>
 </template>
 
 <style scoped>
