f1118dc770e04d9e629ce6da7264b8c0c16a472e
DiegoSeminario <u202412591@upc.edu.pe>
2025-09-26 16:36:27 -0500

    feat: add i18n support for home component

--- src/shared/presentation/views/home.vue
+++ src/shared/presentation/views/home.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
+import {useI18n} from "vue-i18n";
 
+const {t } = useI18n();
 </script>
 
 <template>
+  <div class="align-content-start justify-content-start m-4">
+    <h1>{{ t('home.title') }}</h1>
+    <p>{{ t('home.content') }}</p>
+  </div>
+  
 
 </template>
 
